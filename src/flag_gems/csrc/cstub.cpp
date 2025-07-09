9efc3729d2f77cbd4ec3b05fa57c514336b9eb98
nmpress1 <1935298275@qq.com>
2025-07-09 16:22:48 +0800

    embedding

--- src/flag_gems/csrc/cstub.cpp
+++ src/flag_gems/csrc/cstub.cpp
@@ -22,6 +22,12 @@ TORCH_LIBRARY(flag_gems, m) {
       "rotary_embedding(Tensor q, Tensor k, Tensor cos, Tensor sin, Tensor? position_ids=None, "
       "bool rotary_interleaved=False) -> (Tensor, Tensor)");  // q and k may be view to other size
   m.def("bmm(Tensor self, Tensor mat2) -> Tensor");
+  m.def(
+      "embedding(Tensor weight, Tensor indices, int padding_idx, bool scale_grad_by_freq, bool sparse) -> "
+      "Tensor");
+  m.def(
+      "embedding_backward(Tensor grad_outputs, Tensor indices, SymInt num_weights, int padding_idx, bool "
+      "scale_grad_by_freq, bool sparse) -> Tensor");
 }
 
 TORCH_LIBRARY_IMPL(flag_gems, CUDA, m) {
@@ -35,5 +41,7 @@ TORCH_LIBRARY_IMPL(flag_gems, CUDA, m) {
   m.impl("rotary_embedding", TORCH_FN(rotary_embedding));
   m.impl("rotary_embedding_inplace", TORCH_FN(rotary_embedding_inplace));
   m.impl("bmm", TORCH_FN(bmm));
+  m.impl("embedding", TORCH_FN(embedding));
+  m.impl("embedding_backward", TORCH_FN(embedding_backward));
 }
 }  // namespace flag_gems
