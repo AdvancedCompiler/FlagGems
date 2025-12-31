314c73d23191b08a876f4c4ac8da17903badfb13
songdejun <jude.songd@gmail.com>
2025-12-31 16:38:47 +0800

    move test to a single file

---
+++ tests/test_vllm_ops.py
@@ -0,0 +1,220 @@
+import random
+from itertools import product
+from math import ceil
+from typing import Optional
+
+import pytest
+import torch
+
+import flag_gems
+
+from .conftest import QUICK_MODE
+
+random.seed(42)
+
+
+def is_vllm_available():
+    try:
+        import vllm  # noqa: 401
+
+        return True
+    except ImportError:
+        return False
+
+
+VLLM_AVAILABLE = is_vllm_available()
+
+
+def is_hopper_available():
+    if flag_gems.device != "cuda":
+        return False
+    major, minor = torch.cuda.get_device_capability()
+    sm_version_num = major * 10 + minor
+    return sm_version_num >= 90 and sm_version_num < 100
+
+
+HOPPER_AVAILABLE = is_hopper_available()
+
+
+def to_int8(tensor: torch.Tensor):
+    return torch.round(tensor.clamp(min=-128, max=127)).to(dtype=torch.int8)
+
+
+def to_fp8(tensor: torch.Tensor):
+    finfo = torch.finfo(torch.float8_e4m3fn)
+    return torch.round(tensor.clamp(min=finfo.min, max=finfo.max)).to(
+        dtype=torch.float8_e4m3fn
+    )
+
+
+class CutlassScaledMMTestKit:
+    num_test_cases = 16 if QUICK_MODE else 32
+
+    @classmethod
+    def get_test_params(cls):
+        # these shapes come from the test file of op `cutlass_scaled_mm` of vLLM
+        mnk = [
+            (1, 256, 128),
+            (1, 16384, 1024),
+            (1, 24576, 496),
+            (16, 256, 496),
+            (16, 16384, 128),
+            (16, 24576, 4096),
+            (32, 8192, 4096),
+            (32, 16384, 4096),
+            (33, 1024, 1024),
+            (33, 8192, 128),
+            (64, 2048, 496),
+            (64, 16384, 1024),
+            (100, 8192, 496),
+            (128, 32768, 4096),
+            (256, 4096, 4096),
+            (512, 256, 1024),
+            (512, 8192, 4096),
+            (512, 16384, 128),
+            (512, 24576, 128),
+        ]
+        scale_shape_types = ["scalar", "vector", "matrix"]
+        if_use_bias = [True, False]
+        dtypes = [(torch.int8, torch.float16), (torch.float8_e4m3fn, torch.bfloat16)]
+
+        combinations = product(
+            mnk, scale_shape_types, scale_shape_types, if_use_bias, dtypes
+        )
+
+        all_params = []
+        for (
+            (M, N, K),
+            a_scale_category,
+            b_scale_category,
+            bias,
+            (in_dtype, out_dtype),
+        ) in combinations:
+            is_scalar_or_vector_dequant = a_scale_category in [
+                "scalar",
+                "vector",
+            ] and b_scale_category in ["scalar", "vector"]
+            is_block_dequant = (
+                a_scale_category == "matrix" and b_scale_category == "matrix"
+            )
+
+            if not (is_scalar_or_vector_dequant or is_block_dequant):
+                continue
+
+            if is_block_dequant and (bias is not None or M % 4 != 0):
+                continue
+
+            param = {
+                "M": M,
+                "N": N,
+                "K": K,
+                "a_scale_category": a_scale_category,
+                "b_scale_category": b_scale_category,
+                "use_bias": bias,
+                "in_dtype": in_dtype,
+                "out_dtype": out_dtype,
+            }
+            all_params.append(param)
+        random.shuffle(all_params)
+
+        return all_params[: cls.num_test_cases]
+
+    @staticmethod
+    def baseline_scaled_mm(
+        a: torch.Tensor,
+        b: torch.Tensor,
+        scale_a: torch.Tensor,
+        scale_b: torch.Tensor,
+        out_dtype: torch.dtype,
+        bias: Optional[torch.Tensor] = None,
+    ):
+        def group_broadcast(t: torch.Tensor, shape):
+            for i, s in enumerate(shape):
+                if t.shape[i] != s and t.shape[i] != 1:
+                    assert s % t.shape[i] == 0
+                    t = (
+                        t.unsqueeze(i + 1)
+                        .expand(*t.shape[: i + 1], s // t.shape[i], *t.shape[i + 1 :])
+                        .flatten(i, i + 1)
+                    )
+            return t
+
+        scale_a_full = group_broadcast(scale_a, a.shape)
+        scale_b_full = group_broadcast(scale_b, b.shape)
+
+        a_f32 = a.to(torch.float32)
+        b_f32 = b.to(torch.float32)
+
+        lhs = scale_a_full * a_f32
+        rhs = scale_b_full * b_f32
+
+        output = torch.mm(lhs, rhs).to(out_dtype)
+
+        if bias is not None:
+            output = output + bias
+
+        return output
+
+
+@pytest.mark.skipif(
+    not (VLLM_AVAILABLE and HOPPER_AVAILABLE),
+    reason="requires vLLM and NVIDIA Hopper architecture",
+)
+@pytest.mark.cutlass_scaled_mm
+@pytest.mark.parametrize("p", CutlassScaledMMTestKit.get_test_params())
+def test_cutlass_scaled_mm(p):
+    kit = CutlassScaledMMTestKit
+
+    M, N, K = p["M"], p["N"], p["K"]
+    in_dtype = p["in_dtype"]
+    out_dtype = p["out_dtype"]
+    a_scale_category = p["a_scale_category"]
+    b_scale_category = p["b_scale_category"]
+
+    if in_dtype == torch.int8:
+        a = to_int8(torch.randn((M, K), device=flag_gems.device))
+        b = to_int8(
+            torch.randn((K, N), device=flag_gems.device).t().contiguous().t() * 5
+        )
+    else:
+        a = to_fp8(torch.randn((M, K), device=flag_gems.device))
+        b = to_fp8(torch.randn((K, N), device=flag_gems.device).t().contiguous().t())
+
+    if a_scale_category == "scalar":
+        a_scale_shape = (1, 1)
+    elif a_scale_category == "vector":
+        a_scale_shape = (M, 1)
+    else:
+        a_scale_shape = (M, ceil(K / 128))
+
+    if b_scale_category == "scalar":
+        b_scale_shape = (1, 1)
+    elif b_scale_category == "vector":
+        b_scale_shape = (1, N)
+    else:
+        b_scale_shape = (ceil(K / 128), ceil(N / 128))
+
+    scale_a = torch.randn(a_scale_shape, device=flag_gems.device, dtype=torch.float32)
+    scale_b = torch.randn(b_scale_shape, device=flag_gems.device, dtype=torch.float32)
+
+    scale_a = scale_a.contiguous()
+    # convert scale_b to col-major
+    # (for scalar/vector scale_b, this's a identical transformation)
+    scale_b = scale_b.t().contiguous().t()
+
+    bias = None
+    if p["use_bias"]:
+        bias = torch.randn((N,), device=flag_gems.device, dtype=out_dtype)
+
+    c = torch.empty((M, N), device=flag_gems.device, dtype=out_dtype)
+
+    flag_gems.cutlass_scaled_mm(c, a, b, scale_a, scale_b, bias)
+
+    output_ref = kit.baseline_scaled_mm(a, b, scale_a, scale_b, out_dtype, bias)
+
+    if in_dtype == torch.int8:
+        rtol, atol = 1e-2, 1.0
+    else:
+        rtol, atol = 1e-1, 1.0
+
+    torch.testing.assert_close(c, output_ref, rtol=rtol, atol=atol)
