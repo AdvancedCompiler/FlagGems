7015771225eaf0dd1a8663a3ec53e9024cd191e2
yy33min <2811552420@qq.com>
2026-01-20 11:21:24 +0800

    Move grouped_topk to nvidia/hopper directory and refactor it with non-libdevice syntax to enable multi-chip compatibility

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -13,6 +13,7 @@ from flag_gems.logging_utils import setup_flaggems_logging
 from flag_gems.modules import *  # noqa: F403
 from flag_gems.ops import *  # noqa: F403
 from flag_gems.patches import *  # noqa: F403
+from flag_gems.runtime.backend._nvidia.hopper.grouped_topk import grouped_topk
 from flag_gems.runtime.register import Register
 
 __version__ = "4.2.0"
@@ -479,4 +480,5 @@ __all__ = [
     "enable",
     "only_enable",
     "use_gems",
+    "grouped_topk",
 ]
