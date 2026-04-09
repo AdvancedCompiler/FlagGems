8fbf6b7d5c70fb70c4c4bb76349fe26afcff8156
yy33min <2811552420@qq.com>
2026-04-09 09:35:27 +0800

    fix: add missing import for current_platform

--- tests/test_vllm_ops.py
+++ tests/test_vllm_ops.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 import pytest
 import torch
+from vllm.platforms import current_platform
 
 import flag_gems
 
@@ -13,7 +14,6 @@ from .conftest import QUICK_MODE
 
 try:
     import vllm
-    from vllm.platforms import current_platform
     from vllm.utils.deep_gemm import calc_diff
     from vllm.utils.deep_gemm import (
         fp8_paged_mqa_logits as fp8_paged_mqa_logits_deepgemm,
